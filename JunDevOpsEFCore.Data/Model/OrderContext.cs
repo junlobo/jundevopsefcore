54261d50e2b91949f4e5b66761718105f7ed49d2
Jun <mr.junlobo@gmail.com>
2020-05-28 22:20:20 +1000

    add migration

---
+++ JunDevOpsEFCore.Data/Model/OrderContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JunDevOpsEFCore.Data.Model
+{
+    public class OrderContext : DbContext
+    {
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=StoreDB;");
+        }
+    }
+}
